3cd34016aed36c13ecdc5352d68c66a3bf7a14c9
Pablo Moreno Soza <p.morenosoza@gmail.com>
2025-09-22 00:27:25 -0300

    [fix] - build

---
+++ dist/components/grid/grid-body.d.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import { Task } from "../../types/public-types";
+export declare type GridBodyProps = {
+    tasks: Task[];
+    dates: Date[];
+    svgWidth: number;
+    rowHeight: number;
+    columnWidth: number;
+    todayColor: string;
+    todayLineColor: string;
+    rtl: boolean;
+};
+export declare const GridBody: React.FC<GridBodyProps>;
